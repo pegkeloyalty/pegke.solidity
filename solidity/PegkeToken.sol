a7405f7d22f892fdf5f81af1ae4e0ac209ec6119
sibizulu <sibiraj@lightrains.com>
2016-06-09 20:20:20 +0530

    Adding name and symbol

--- solidity/PegkeToken.sol
+++ solidity/PegkeToken.sol
@@ -2,12 +2,16 @@ contract PegkeToken {
   
   uint256 public totalTokens;
   uint256 initialTokens;
+  string public tokenName;
+  string public tokenSymbol;
   
   mapping (address => uint256) public balanceOf;
   
   function PegkeToken (){
     initialTokens = 10000;
-    totalTokens = 10000;
+    totalTokens = initialTokens;
+    tokenName = "PegkeCoin";
+    tokenSymbol = "℗";
     balanceOf[msg.sender] = initialTokens; 
   }
 }
